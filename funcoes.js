a536e179777f1433f257c9d2321d6634a1dfe1d1
Johnny Santos <firevendas@firevendas.com.br>
2015-09-03 16:22:27 -0300

    Login

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,35 @@
 $(function() {
+	
 	$('#logo').fadeIn(2000);
+	
+	var email = localStorage.getItem('fv.email_firevendas');
+	
+	if( email == null ){
+		$('#btn-primeiro-acesso').show();
+	}else{
+		$('#btn-acesso').show();
+	}
 });
+
+function gravarNovo(){
+	
+	if( $("#email-representacao").val() != '' && 
+		$("#nome").val() != '' && 
+		$("#telefone").val() != '' && 
+		$("#email-firevendas").val() != '' && 
+		$("#senha-firevendas").val() != '' && 
+		$("#empresa").val() != '' ){
+				
+		localStorage.setItem('fv.representacao', $("#email-representacao").val() );
+		localStorage.setItem('fv.nome', $("#nome").val() );
+		localStorage.setItem('fv.telefone', $("#telefone").val() );
+		localStorage.setItem('fv.email_firevendas', $("#email-firevendas").val() );
+		localStorage.setItem('fv.senha_firevendas', $("#senha-firevendas").val() );
+		localStorage.setItem('fv.empresa', $("#empresa").val() );
+		
+		window.location = 'index.html';
+		
+	}else{
+		alert('É necessário o preenchimento de todos os dados');	
+	}
+}
